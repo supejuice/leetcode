7592969a00b3a4f2ef176edadb9e83439035b81c
nithin <sysalchemist@hotmail.com>
2023-02-14 17:47:38 +0530

    accepted 121

--- src/main/kotlin/121.kt
+++ src/main/kotlin/121.kt
@@ -4,11 +4,11 @@ fun main() {
 
 private fun maxProfit(prices: IntArray): Int {
     var maxProfit = 0
-    for (i in 0 until prices.lastIndex) {
-        for (j in i + 1 until prices.size) {
-            val profit = prices[j] - prices[i]
-            if (profit > maxProfit) maxProfit = profit
-        }
+    var lowestPrice = prices.first()
+    prices.forEach { price ->
+        if (price < lowestPrice) lowestPrice = price
+        val profit = price - lowestPrice
+        if (profit > maxProfit) maxProfit = profit
     }
     return maxProfit
 }
